69d65b1765c9f3c6fb2c83dfdc96e75e91f13eb5
AldairPerezv <129784268+AldairPerezv@users.noreply.github.com>
2024-03-05 23:28:39 -0500

    Terminado video(9)
    Video 9 se basa en crear un login obtener login, creacion de crud de cargo y instalacion de libreria boostrap mediante consola gitbash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CargoRequest } from './models/cargo-request.model';
 import { AuthService } from './services/auth.service';
+import { CargoService } from './services/cargo.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,8 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit, AfterViewInit {
 
-  usuario = "";
-  password = "";
+  usuario = "admin";
+  password = "123456";
   nombreUsuario = "NOMBRE USUARIO";
   title = 'Eiby Moda';
 
@@ -18,11 +19,12 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   //declaramos la variable toke
   token: string = "";
-
+  cargoRequest: CargoRequest = new CargoRequest();
   cargos: CargoRequest[] = [];
 
   constructor(
-    private _authService: AuthService
+    private _authService: AuthService,
+    private _cargoService: CargoService
   ) {
 
   }
@@ -66,7 +68,8 @@ export class AppComponent implements OnInit, AfterViewInit {
   /**
    * TODO: COMO PARAMETRO SE DEBE ENVIAR EL TOKEN OBTENIDO AL MOMENTO DE INICIAR SESIÓN
    */
-  listarRoles() {    
+  listarRoles() {  
+    this.cargos = [];
     console.log("token ==> ", this.token);   
     this._authService.roles(this.token).subscribe(
       {
@@ -80,7 +83,59 @@ export class AppComponent implements OnInit, AfterViewInit {
         complete: () => { }
       }
     );
+  }
+
+  crearCargo() {
+    
+    if (this.cargoRequest.id == 0) {
+
+      this._cargoService.create(this.token, this.cargoRequest).subscribe({
+        next: () => {
+          alert("CREADO");
+          this.listarRoles();
+        },
+        error: () => {
+          alert("ocurrio un error");
+        },
+        complete: () => { }
+      });
+    }
+    else {
+            this._cargoService.update(this.token, this.cargoRequest).subscribe({
+        next: () => {
+          alert("ACTUALIZADO");
+          this.listarRoles();
+        },
+        error: () => {
+          alert("ocurrio un error");
+        },
+        complete: () => { }
+      });
+    }
 
 
   }
+
+  rellenarValores(cargo:CargoRequest) {
+    this.cargoRequest = cargo;
+
+  }
+
+  eliminarRegistro(id: number) {
+
+    let eliminar = confirm("¿Estas seguro de eliminar?");
+    if (eliminar) {
+      this._cargoService.delete(this.token, id).subscribe({
+        next: () => {
+          alert("ELIMINADO");
+          this.listarRoles();
+        },
+        error: () => {
+          alert("ocurrio un error");
+        },
+        complete: () => { }
+      });
+    }
+  }
+
 }
